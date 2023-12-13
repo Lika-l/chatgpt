ae8df696871cf3eb4793ca3304c229f0e7ddc505
lika lanovskaya <anshelika.grushanskaya@gmail.com>
2023-12-13 13:56:39 +0200

    Updates

---
+++ precache-manifest.c3422ca36b191df90842e8849ee43418.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a94f36fe579b327ceff385a1cf29be",
+    "url": "/chatgpt/index.html"
+  },
+  {
+    "revision": "10b8f9df893ca6a3e1f7",
+    "url": "/chatgpt/static/css/main.2e044a0f.chunk.css"
+  },
+  {
+    "revision": "5cae19ec9c50db945743",
+    "url": "/chatgpt/static/js/2.ad611bcc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/chatgpt/static/js/2.ad611bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b8f9df893ca6a3e1f7",
+    "url": "/chatgpt/static/js/main.802ebaf2.chunk.js"
+  },
+  {
+    "revision": "9df36b43e038771fa751",
+    "url": "/chatgpt/static/js/runtime-main.ff5b007d.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/chatgpt/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/chatgpt/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/chatgpt/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/chatgpt/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/chatgpt/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/chatgpt/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/chatgpt/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/chatgpt/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/chatgpt/static/media/possibility.3c84ae26.png"
+  }
+]);
